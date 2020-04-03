bb18c8015dfe872ca62b0a5c318cb04d6269c6f4
Sebsterio <sebastian.rosloniec@gmail.com>
2020-04-03 17:40:30 +0200

    style product description on product page

--- client/src/pages/product/product.component.jsx
+++ client/src/pages/product/product.component.jsx
@@ -12,7 +12,7 @@ import { AddButton } from "./product.styles";
 
 // -------------------------------------------------------------------------
 
-// TODO: movile-view: images align center, not top
+// TODO: mobile-view: images align center, not top
 
 export const ProductPage = ({ product, addItem }) => {
 	const [collapsed, setCollapsed] = useState(false);
@@ -20,6 +20,7 @@ export const ProductPage = ({ product, addItem }) => {
 	if (!product) return <Redirect to="/page-missing" />;
 	const {
 		name,
+		price,
 		color,
 		customColor,
 		description,
@@ -41,26 +42,58 @@ export const ProductPage = ({ product, addItem }) => {
 		</span>
 	);
 	if (customColor) descriptionHtml.push(customColorText);
+	const help = "need help?";
 
 	return (
 		<div className="ProductPage">
 			<div className="wrapper">
 				<div className="pic" onClick={() => setCollapsed(!collapsed)}>
-					{/* <img src={imagesArr[0]} alt="garment" /> */}
 					{imagesHtml}
 				</div>
 				<div className={`desc-dock ${collapsed ? "collapsed" : ""}`}>
 					<div className="desc-container">
-						<div className="desc Desc">
-							<h2>{name}</h2>
-							<p>{type}</p>
-							<p>{color}</p>
-							<p>size...</p>
+						<div className="desc">
+							<header>
+								<span className="heading">{name}</span>
+								<span className="price">&euro;{price}</span>
+							</header>
+							<div className="sub-heading">
+								<span className="type">{type}</span>
+								<span className="color">{color}</span>
+							</div>
+							<div className="form">
+								<select name="size" id="select-size" className="input">
+									<option value="XS">XS</option>
+									<option value="S">S</option>
+									<option value="M" selected>
+										M
+									</option>
+									<option value="L">L</option>
+								</select>
+								<select name="quantity" id="select-quantity" className="input">
+									<option value="1" selected>
+										1
+									</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+								</select>
+							</div>
 							<AddButton onClick={() => addItem(product)} inverted>
 								Add to bag
 							</AddButton>
-							<p>{descriptionHtml}</p>
-							<div>prev | next</div>
+
+							<div className="description">{descriptionHtml}</div>
+							<div className="help">
+								<span>{help}</span>
+							</div>
+							<div className="nav-buttons">
+								<span className="nav-btn prev">prev</span>
+								<span className="nav-btn separator"> | </span>
+								<span className="nav-btn next">next</span>
+							</div>
 						</div>
 					</div>
 				</div>
--- client/src/pages/product/product.styles.jsx
+++ client/src/pages/product/product.styles.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import CustomButton from "../../components/custom-button/custom-button.component";
 
-export const AddButton = styled(CustomButton)``;
+export const AddButton = styled(CustomButton)`
+	width: 100%;
+`;
 
 AddButton.displayName = "AddButton";
