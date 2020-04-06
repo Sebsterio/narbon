ea89e82d39d66f28d0336ba21b9ecf6a0614ec8d
Sebsterio <sebastian.rosloniec@gmail.com>
2020-04-06 14:25:25 +0200

    button design

--- client/src/components/custom-button/custom-button.styles.jsx
+++ client/src/components/custom-button/custom-button.styles.jsx
@@ -1,39 +1,38 @@
 import styled, { css } from "styled-components";
 
 const buttonStyles = css`
-	background-color: pink;
-	color: white;
-	border: none;
-
-	&:hover {
-		background-color: white;
-		color: black;
-		border: 1px solid black;
-	}
+	background-image: linear-gradient(
+		to right,
+		#29323c,
+		#485563,
+		#2b5876,
+		#4e4376
+	);
+	box-shadow: 0 4px 15px 0 rgba(45, 54, 65, 0.75);
 `;
 
 const invertedButtonStyles = css`
-	background-color: white;
-	color: black;
-	border: 1px solid black;
-
-	&:hover {
-		background-color: pink;
-		color: white;
-		border: none;
-	}
+	background-image: linear-gradient(
+		to right,
+		#ed6ea0,
+		#ec8c69,
+		#f7186a,
+		#fbb03b
+	);
+	box-shadow: 0 4px 15px 0 rgba(236, 116, 149, 0.75);
 `;
 
 const googleSignInStyles = css`
-	background-color: #4285f4;
-	color: white;
-	border: none;
-	font-size: 12px;
-	line-height: 20px;
-
-	&:hover {
-		background-color: #357ae8;
-	}
+	font-size: 13px;
+	letter-spacing: 0;
+	background-image: linear-gradient(
+		to right,
+		#25aae1,
+		#4481eb,
+		#04befe,
+		#3f86ed
+	);
+	box-shadow: 0 4px 15px 0 rgba(65, 132, 234, 0.75);
 `;
 
 const getButtonStyles = (props) => {
@@ -48,17 +47,30 @@ export const CustomButtonContainer = styled.button`
 	min-width: 165px;
 	width: auto;
 	height: 50px;
-	letter-spacing: 0.5px;
-	line-height: 50px;
 	padding: 0 35px 0 35px;
 	font-size: 15px;
+	line-height: 17px;
+	letter-spacing: 0.5px;
 	text-transform: uppercase;
 	font-family: "Josefin Sans", "Open Sans", "Helvetica Neue", "Helvetica",
 		"sans-serif";
 	font-weight: bold;
-	cursor: pointer;
 	display: flex;
 	justify-content: center;
+	cursor: pointer;
+
+	color: white;
+	border: none;
+	background-size: 300% 100%;
+	transition: all 0.4s ease-in-out;
 
 	${getButtonStyles}
+
+	&:hover {
+		background-position: 100% 0;
+	}
+
+	&:focus {
+		outline: none;
+	}
 `;
